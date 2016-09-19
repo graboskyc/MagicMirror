944bf8639adfdab848eb7988cb02d3a1acd3b244
Chris Grabosky <chris@grabosky.net>
2016-09-19 07:54:13 -0400

    Modified it to pull latest from wunderground every 15 min to reduce api consumption

--- weather.php
+++ weather.php
@@ -6,8 +6,21 @@ class Weather {
     public function __construct() {
         $key = "";
         $loc = "NJ/Cherry_Hill";
-        #$json_current = file_get_contents("http://api.wunderground.com/api/".$key."/conditions/q/".$loc.".json");
-        $json_current = file_get_contents("./current.json");
+
+        $filename = "./current.json";
+
+        if (time()-filemtime($filename) > (60*15)) {
+            // file older than 15 min
+            $json_current = file_get_contents("http://api.wunderground.com/api/".$key."/conditions/q/".$loc.".json");
+            
+            $fh = fopen($filename, "w") or die("Unable to open file!");
+            fwrite($fh, $json_current);
+            fclose($fh);
+        } else {
+            // file younger than 15 min
+            $json_current = file_get_contents($filename);
+        }
+        
         $current = json_decode($json_current);
 
         $this->CurrIcon = $current->current_observation->icon;
