c865d40629c8e98828f2ac8966ee23b544edbec4
Chris Grabosky <chris@grabosky.net>
2016-09-26 10:22:47 -0400

    Better formatting of delays

--- SERVER/modules/FAA.php
+++ SERVER/modules/FAA.php
@@ -4,6 +4,7 @@ class FAA {
     public $DelayType = "";
     public $Loc = "";
     public $MaxDelay = "";
+    public $AvgDelay = "";
 
     public function __construct($aircode) {
         $this->Loc = $aircode;
@@ -24,8 +25,8 @@ class FAA {
         $current = json_decode($json_current);
         $this->Delay = $current->delay;
         $this->DelayType = $current->status->type;
-        $this->MaxDelay = $current->status->maxDelay;
-
+        $this->MaxDelay = str_replace("minute", "min", str_replace("hour", "hr", $current->status->maxDelay));
+        $this->AvgDelay = str_replace("minute", "min", str_replace("hour", "hr", $current->status->avgDelay));
     }
 
     public function Draw() {
@@ -34,17 +35,21 @@ class FAA {
         echo "<div style='float:left;width:55%;'>";
 
         $icon = "ThumbsUp.png";
+        $msg = "";
         if ($this->Delay == "true") {
-            if ($this->DelayType == "Airport Closure") { $icon = "Close.png"; $msg = $this->DelayType; }
-            else if ($this->DelayType == "Ground Stop") { $icon = "Stop.png"; $msg = $this->DelayType; }
-            else if ($this->DelayType == "Ground Delay") { $icon = "Timer.png"; $msg = $this->DelayType; }
-            else { $icon = "Question.png"; $msg = $this->DelayType;}
+            if ($this->DelayType == "Airport Closure") { $icon = "Close.png"; $msg = "Closed"; }
+            else if ($this->DelayType == "Ground Stop") { $icon = "Stop.png"; $msg = "Grnd Stp"; }
+            else if ($this->DelayType == "Ground Delay") { $icon = "Timer.png"; $msg = "Grnd Del"; }
+            else { $icon = "Question.png"; $msg = str_replace("Departure", "Dep", $this->DelayType);}
+
+            if($this->MaxDelay !== "") { $msg = $msg . " " . $this->MaxDelay . " max";}
+            else if  ($this->AvgDelay !== "") { $msg = $msg . " " . $this->AvgDelay . " avg";}
         }
         else {
             $msg = "";
         }
         
-        echo "<img width='24' height='24' src='icons/".$icon."' style='margin-right:18px;'/><span style='font-size:12px;'>" . $msg . " " .$this->MaxDelay . "</span>";
+        echo "<img width='24' height='24' src='icons/".$icon."' style='margin-right:18px;'/><span style='font-size:12px;'>" . $msg . "</span>";
         echo "</div></div>";
     }
 }
